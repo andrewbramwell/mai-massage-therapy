fc845bbcb7db96feb86c47537318ec90c68ad56f
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-02-16 10:33:47 +0000

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "name": "Patthira Thai Spa",
+    "email": "patthirathaispa@gmail.com",
+    "phoneForTel": "+441604946448",
+    "phoneFormatted": "01604 946448",
+    "openingHours": "Mon-Sat: 10am-8pm",
+    "onlineBooking": "https://patthirathaispa.setmore.com/",
+    "address": {
+        "lineOne": "34 York Rd",
+        "lineTwo": "",
+        "city": "Northampton",
+        "state": "",
+        "zip": "NN1 5QH",
+        "country": "UK",
+        "mapLink": "https://maps.app.goo.gl/oNukY9Las2WTSCdT8"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/profile.php?id=61572085995736",
+        "whatsapp": "https://wa.me/+447947424114",
+        "googleReview": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://patthirathaispa.co.uk"
+};
+
+
