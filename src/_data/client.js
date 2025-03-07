adace913e977dd50d7d34c076b99ae03c5b04c31
andrewbramwell <andy@voucherstore.io>
2025-03-07 20:37:26 +0000

    update

--- src/_data/client.js
+++ src/_data/client.js
@@ -3,7 +3,7 @@ module.exports = {
     "email": "mai.massagetherapyservice@gmail.com",
     "phoneForTel": "+447359925280",
     "phoneFormatted": "07359 925280",
-    "openingHours": "Mon: 3pm-8pm | Wed Thu Fri: 10am-8pm",
+    "openingHours": "Mon: 3pm-7pm | Wed Thu Fri: 10am-7pm",
     "onlineBooking": "https://maismassagetherapy.setmore.com",
     "address": {
         "lineOne": "99A Canterbury Road",
@@ -12,7 +12,7 @@ module.exports = {
         "state": "",
         "zip": "CT5 4HG",
         "country": "UK",
-        "mapLink": ""
+        "mapLink": "https://maps.app.goo.gl/FaTw5NcnYLDhNSkz8"
     },
     "socials": {
         "facebook": "https://www.facebook.com/profile.php?id=61567496233775",
