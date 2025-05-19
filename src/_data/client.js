6b738aab3fe73fe554e54c7184d551c096d6e44f
andrewbramwell <andy@voucherstore.io>
2025-05-19 19:25:37 +0100

    update hours

--- src/_data/client.js
+++ src/_data/client.js
@@ -3,7 +3,7 @@ module.exports = {
     "email": "mai.massagetherapyservice@gmail.com",
     "phoneForTel": "+447359925280",
     "phoneFormatted": "07359 925280",
-    "openingHours": "Mon: 3pm-7pm | Tues - Fri: 10am-7pm",
+    "openingHours": "Mon-Fri 10am-7pm",
     "onlineBooking": "https://www.sumupbookings.com/mais-massage-therapy",
     "address": {
         "lineOne": "99A Canterbury Road",
